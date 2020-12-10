12c9b6d9057a9b2dec549a9705ab149916d310ac
Juan Navarro <jupanze@gmail.com>
2020-12-09 20:50:44 -0500

    Adicion modulo cliente

--- Restaurante-Api/db/cliente_db.py
+++ Restaurante-Api/db/cliente_db.py
@@ -1,3 +1,4 @@
+from datetime import date
 from typing import Dict
 from pydantic import BaseModel
 
@@ -7,25 +8,25 @@ class ClienteInDB(BaseModel):
     direccion: str
     barrio: str
     cedula: str
-    cumpleanos: str
+    cumpleanos: date
 
 
 database_clientes = {
-    "3214567895": ClienteInDB(**{"telefono":"3214567895",
-                            "nombre":"Pepe",
-                            "direccion":"Calle 12 # 20 - 22",
-                            "barrio":"Colombia",
-                            "cedula":"456123789",
-                            "cumpleanos":"01-Feb-80"
-                            }),
+    3214567895: ClienteInDB(**{"telefono": 3214567895,
+                                "nombre":"Pepe",
+                                "direccion":"Calle 12 # 20 - 22",
+                                "barrio":"Colombia",
+                                "cedula":"456123789",
+                                "cumpleanos": '1980-2-20'
+                                }),
 
-    "3007894561": ClienteInDB(**{"telefono":"3007894561",
-                            "nombre":"Pepita",
-                            "direccion":"Carrera 20 # 15 - 12",
-                            "barrio":"Parnaso",
-                            "cedula":"1123456789",
-                            "cumpleanos":"10-Ene-85"
-                            }),
+    3007894561: ClienteInDB(**{"telefono": 3007894561,
+                                "nombre":"Pepita",
+                                "direccion":"Carrera 20 # 15 - 12",
+                                "barrio":"Parnaso",
+                                "cedula":"1123456789",
+                                "cumpleanos": '1985-1-10'
+                                }),
 }
 
 def get_cliente(telefono: int):
@@ -39,5 +40,12 @@ def update_cliente(cliente_in_db: ClienteInDB):
     return cliente_in_db
 
 def create_cliente(nuevo_cliente: ClienteInDB):
-    database_clientes.append(nuevo_cliente)
-    return database_clientes[nuevo_cliente.keys()]
+    database_clientes[nuevo_cliente.telefono] = nuevo_cliente
+    return nuevo_cliente
+
+def eliminate_cliente(cliente: ClienteInDB):
+    del database_clientes[cliente.telefono]
+    return "Se elimino satisfactoriamente"
+
+def get_all_clientes():
+    return database_clientes.values()
